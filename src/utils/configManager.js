151aefaa2d45949f9c176c31686dabe1baefa39f
Jeff404Studio <jf@genefty.com>
2026-01-06 11:06:24 +0100

    maj configManager

--- src/utils/configManager.js
+++ src/utils/configManager.js
@@ -5,13 +5,13 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const CONFIG_FILE = path.join(__dirname, '../../data/config.json');
 
-// Valeurs par défaut (au cas où le fichier est vide)
 const defaultConfig = {
     channels: {
         announce: null,
         spotlight: null,
         feedback: null,
-        logs: null
+        logs: null,
+        welcome: null 
     }
 };
 
@@ -25,9 +25,19 @@ export function getConfig() {
     }
 }
 
+export function getChannel(type) {
+    const config = getConfig();
+    return config.channels[type] || null;
+}
+
 export function setChannel(type, channelId) {
     const config = getConfig();
+    if (!config.channels) config.channels = {}; // Sécurité
     config.channels[type] = channelId;
+    
+    const dir = path.dirname(CONFIG_FILE);
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    
     fs.writeFileSync(CONFIG_FILE, JSON.stringify(config, null, 2));
     return true;
 }
